ee205fc3d3b5dab80d80e4f2b0b0a6a9b2ff64f3
Pedro Campelo Matheus <pedrohcmatheus@gmail.com>
2021-08-09 09:54:34 -0300

    change

---
+++ format.js
@@ -0,0 +1,13 @@
+const contacts = document.getElementById('contacts');
+const excel = document.getElementById('excel');
+const indicator = document.getElementById('indicator');
+const indicatorPhone = document.getElementById('indicatorPhone');
+const commentary = document.getElementById('commentary');
+
+
+
+
+
+// function of button 1
+// function of button 2
+// function of button 3
